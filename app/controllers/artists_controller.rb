eefd86b6434adb391435a3178559a5479fa3a170
marsha <marshaannon@gmail.com>
2020-05-11 12:48:49 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,15 @@
 class ArtistsController < ApplicationController
+
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    if !@artist
+      redirect_to artists_path
+    end
   end
 
   def new
@@ -11,7 +18,6 @@ class ArtistsController < ApplicationController
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -20,14 +26,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -36,7 +38,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -47,4 +48,9 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def set_artist
+    @artist = Artist.find_by_id(params[:id])
+  end
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,15 @@
 class SongsController < ApplicationController
+
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
+    @songs = Song.all
   end
 
   def show
+    if !@song
+      redirect_to songs_path
+    end
   end
 
   def new
@@ -12,7 +19,6 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     @song.artist = Artist.find_or_create_by(artist_params)
-
     if @song.save
       redirect_to @song
     else
@@ -21,14 +27,10 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-
     @song.update(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -37,7 +39,6 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -52,4 +53,9 @@ class SongsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def set_song
+    @song = Song.find_by_id(params[:id])
+  end
+
 end
